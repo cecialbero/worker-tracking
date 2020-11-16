c5da6f088d381fa7d30bba365d4c8f6140352ec0
claudio pedalino <claudio.dpedalino@gmail.cmo>
2020-11-16 03:44:12 -0300

    add environment base url add status-service [connect endpoint get-all] and status model

--- src/app/components/other-users-info/other-users-info.component.ts
+++ src/app/components/other-users-info/other-users-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Status } from 'src/app/shared/models/status.model';
+import { StatusService } from 'src/app/shared/services/status.service';
 import { Data, OtherUsersInfoResponse } from './models/other-users-info.model';
 import { OtherUsersInfoService } from './other-users-info.service';
 
@@ -11,52 +13,36 @@ export class OtherUsersInfoComponent implements OnInit {
 
   public currentTime = `${new Date().getDay()}/${new Date().getMonth()} - ${new Date().getHours()}:${new Date().getMinutes()}hs`;
   public otherWorkers: Data[];
+  public statusList = new Array<Status>();
 
-  workers = [
-    {
-      firstName: 'Bart',
-      lastName: 'Samali',
-      avatar: null,
-      role: 'Project Manager',
-      team: ['Tokio', 'Alaska'],
-      email: 'bart-samali@companyname.com',
-      time: this.currentTime
-    },
-    {
-      firstName: 'John',
-      lastName: 'Skyler Moore',
-      avatar: 'avatar.png',
-      role: 'Full-stack Developer',
-      team: ['Tokio'],
-      email: 'j-skylermoore@companyname.com',
-      time: this.currentTime
-    },
-    {
-      firstName: 'Samantha',
-      lastName: 'Lopez Garcia',
-      avatar: null,
-      role: 'Project Manager',
-      team: ['Tokio', 'Alaska', 'Roma'],
-      email: 's-lopezgarcia@companyname.com',
-      time: this.currentTime
-    }
-  ];
-
-  status = ['Active', 'Away', 'Offline', 'Busy'];
+  status = ['Active', 'Away', 'Offline', 'Busy']; // TODO:connect service
 
   constructor(
-      public otherUsersInfoService: OtherUsersInfoService
+      public otherUsersInfoService: OtherUsersInfoService,
+      public statusService: StatusService,
     ) { }
 
   public getTeamsNames(teams: any): string {
     return teams.map(x => x.name);
   }
 
+  public getAllStatus(): Array<Status> {
+    this.statusService.getAllStatus().subscribe(response => {
+      return response.map(x => x.statusName);
+    },
+    error => {
+      console.log(error);
+    });
+    return this.statusList;
+  }
+
   ngOnInit(): void {
+    // this.getAllStatus();
     this.otherUsersInfoService.getOtherUsersInfo().subscribe( response => {
       this.otherWorkers = response.data;
     },
-    error => {console.log(error);
+    error => {
+      console.log(error);
     });
   }
 
--- src/app/components/other-users-info/other-users-info.service.ts
+++ src/app/components/other-users-info/other-users-info.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OtherUsersInfoResponse } from './models/other-users-info.model';
 import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
 
-const API_URL = 'https://localhost:44351/api/';
+const API_URL = environment.API_URL;
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,6 @@ export class OtherUsersInfoService {
   constructor(private http: HttpClient) {
   }
 
-  // tslint:disable-next-line: typedef
   public getOtherUsersInfo(): Observable<OtherUsersInfoResponse> {
     return this.http.get<OtherUsersInfoResponse>(`${API_URL}Worker/get-all`);
   }
